a966db32f562fa359c9d849cf867ba10c71b784c
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2019-04-06 01:26:41 +0500

    adding the scripts (start & server)

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello"));
+app.get("/", (req, res) => res.send("Hello !!"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running on port ${port}`));
