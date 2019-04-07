ee70c8b0844568a947cb5422f6d808f3f4b44c32
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2019-04-08 00:24:06 +0500

    Route Files With Express Router

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
 const app = express();
 //DB config
 const db = require("./config/Keys").mongoURI;
 mongoose
   .connect(db)
-  .then(()=>console.log("Mongo DB connected"))
+  .then(() => console.log("Mongo DB connected"))
   .catch(err => console.log(err));
 app.get("/", (req, res) => res.send("Hello World!"));
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running on port ${port}`));
