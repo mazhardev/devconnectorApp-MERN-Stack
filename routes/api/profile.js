d346e95064f33aefeaf70f75376ebc714e09059b
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2019-07-27 16:23:35 +0500

    Current User Profile Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const Profile = require("../../models/Profile");
+const passport = require("passport");
 
 //@route    GET api/profile/test
 //@desc     Tests profile route
 //@access   Public
-
-
 router.get("/test", (req, res) => res.json({ msg: "profile works" }));
+
+//@route    GET api/profile
+//@desc     Get current user profile
+//@access   Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = "There is no profile for this user!";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
