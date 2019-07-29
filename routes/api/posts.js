564a32ae12bbe76abf746bcfec8e813514b939b3
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2019-07-29 11:39:57 +0500

    Post Create Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const passport=require('passport');
+// Post model
+const Post = require('../../models/post');
 
 //@route    GET api/posts/test
 //@desc     Tests posts route
 //@access   Public
-
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+    
+      const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      });
+  
+      newPost.save().then(post => res.json(post));
+    }
+  );
+  
+  
 module.exports = router;
