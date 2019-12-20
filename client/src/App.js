c198eff026ea574218979fbbde862b486fe7a4fd
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2019-12-20 22:50:03 +0500

    changing in App.js and index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
-import { Route } from "react-router-dom";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './app/store/configureStore';
 
+import jwt_decode  from "jwt-decode";
+import setAuthToken from "./app/common/util/setAuthToken";
+import {setCurrentUser} from "./components/auth/authActions";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <div className="container">
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </div>
-      <Footer />
-    </div>
-  );
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from './app/store/configureStore';
+
 ReactDOM.render(
-  <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Provider>,
+  <App />,
   document.getElementById("root")
 );
 
